8415ed250b7c8d7c882c9cdc0dbaf482d62333e2
Kipouralkis <38573852+Kipouralkis@users.noreply.github.com>
2025-11-01 23:09:22 +0200

    added card styling

--- memory-game/src/components/content/Card.jsx
+++ memory-game/src/components/content/Card.jsx
@@ -4,8 +4,9 @@ function Card(props) {
   return (
     <>
     <div className={`card-body ${props.layoutClass}`}>
-      <h1>CARD</h1>
-      <img src={props.imgURL} alt={props.name}></img>
+      <div className='pokeimg'>
+         <img src={props.imgURL} alt={props.name}></img>
+      </div>
       <p>{props.name}</p>
     </div>
     </>
