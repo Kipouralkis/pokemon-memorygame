e6c454d2fb3e601385c462e9791437c5eb70f12b
Kipouralkis <38573852+Kipouralkis@users.noreply.github.com>
2025-11-10 21:01:23 +0200

    styled options butotn

--- memory-game/src/components/Header.jsx
+++ memory-game/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import '../styles/header.css'
+import OptionsButton from './options/OptionsButton';
 
 function Header(props) {
   return (
     <header>
-      <h1>Gotta Catch Them All!! (only once !!)</h1>
-      <br />
-      <p>Score: {props.score}</p>
-      <p>High Score: {props.highScore}</p>
+      <div className='header-text'>
+        <h1>Gotta Catch Them All!! (only once !!)</h1>
+        <br />
+        <p>Score: {props.score}</p>
+        <p>High Score: {props.highScore}</p>
+      </div>
+      <OptionsButton />
     </header>
   );
 }
---
+++ memory-game/src/components/options/OptionsButton.jsx
@@ -0,0 +1,9 @@
+import '../../styles/options.css'
+
+function OptionsButton(props){
+    return(
+        <button className='options-button'>Options</button>
+    )
+}
+
+export default OptionsButton;
